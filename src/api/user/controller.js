8933cd8d6c7af9356bfaa6b6b3596ce5d95cf319
storme-dev <k_karpitskiy@mail.ru>
2022-06-16 13:15:24 +0300

    first commit

---
+++ src/api/user/controller.js
@@ -0,0 +1,15 @@
+const userService = require('./service');
+
+module.exports = {
+    /*
+    async getTree(req, res) {
+        try {
+            const tree = await citizenService.getTree();
+            res.status(200).send({
+                data: tree
+            });
+        } catch (e) {
+            res.error(e);
+        }
+    },*/
+};
---
+++ src/api/user/router.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const userRouter = express.Router();
+const userController = require('./controller');
+
+module.exports = userRouter;
